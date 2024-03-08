5c40162edbb070d2b12e00cb4834095cb426bc3a
Alejandro Giorgio <alegior7@gmail.com>
2024-03-07 21:54:07 -0300

    reentrega

---
+++ constants.py
@@ -0,0 +1,19 @@
+date_columns = [
+    "incident_datetime",
+    "incident_date",
+    "incident_time",
+    "report_datetime",
+]
+
+string_columns = [
+    "incident_day_of_week",
+    "report_type_code",
+    "report_type_description",
+    "incident_category",
+    "incident_subcategory",
+    "incident_description",
+    "resolution",
+    "police_district",
+    "intersection",
+    "analysis_neighborhood",
+]
